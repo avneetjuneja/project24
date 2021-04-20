26ec6d0015694da4ef019af1d971e999d9281d12
avneetjuneja <69860291+avneetjuneja@users.noreply.github.com>
2021-04-20 12:30:55 +0530

    Update dustbin.js

--- dustbin.js
+++ dustbin.js
@@ -1,21 +1,22 @@
-class DustBin {
-    constructor(x, y, width, height){
-        var options = {
-            'isStatic' : true
-        }
+class Dustbin {
+    constructor(x, y, width, height) {
+      var options = {
+          isStatic: true,
+          'restitution':0,
+          'friction':1,
+          'density':0.1
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      rectMode(CENTER);
 
-        this.Bodyb = Bodies.rectangle(x, y, width, height, options);
-        World.add(world, this.Bodyb);
-        this.body1 = Bodies.rectangle((x- (width/2)), (y- (height*2)), )
-        World.add(world, this.body1);
-        this.bodyr = Bodies.rectangle((x+(width/2)), (y-(height*2)), height, width/2, options);
-        world.add(world, this.bodyr);
-        this.bodyb.width = width
-        this.bodyb.height = height
+      fill(255);
+
+      rect(this.body.position.x, this.body.position.y, this.width, this. height);
     }
-    display() {
-        fill("white");
-        rect(this.bodyb.position.x, this.bodyb.position.y, this.bodyb.width, this.bodyb.height)
-        rect((this.bodyb.position.x-(this.bodyb.position.y-(this.bodyb.height*(2*(this.bodyb.width/100))), this.bodyb.height, this.bodyb.width/2))
-    }
-}
+  };
